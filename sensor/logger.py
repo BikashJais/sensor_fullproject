a61d12957d2a1507ad114d7059cb553ff5241106
Bikash <bikoojais52@gmail.com>
2023-03-15 01:12:28 +0530

    created some folders and logger file

---
+++ sensor/logger.py
@@ -0,0 +1,21 @@
+import os
+import logging
+from datetime import datetime
+
+
+#creating log file name
+LOG_FILE_NAME= f"{datetime.now().strftime('%m%d%Y__%H%M%Y')}.log"
+
+#creating logs directory/Folder
+
+LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
+
+#creating whole  log file path
+
+LOG_FILE_PATH= os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
